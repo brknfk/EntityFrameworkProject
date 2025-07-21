7354cd3b1fc1c33e6514810d13a87d9d07158c5e
brknfk <fakilarberkan@gmail.com>
2025-07-21 14:33:35 +0300

    yeni

---
+++ FrmProduct.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace EntityFrameworkProject
+{
+    partial class FrmProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtProductId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnList = new System.Windows.Forms.Button();
+            this.txtProductStock = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtProductPrice = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbProductCategory = new System.Windows.Forms.ComboBox();
+            this.btnListWithCategory = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(121, 433);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(157, 37);
+            this.btnSearch.TabIndex = 19;
+            this.btnSearch.Text = "Ara";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(121, 390);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(157, 37);
+            this.btnUpdate.TabIndex = 18;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(121, 347);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(157, 37);
+            this.btnDelete.TabIndex = 17;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(121, 304);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(157, 37);
+            this.btnAdd.TabIndex = 16;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(121, 86);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(157, 22);
+            this.txtProductName.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Ürün Adı:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(300, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(826, 429);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // txtProductId
+            // 
+            this.txtProductId.Location = new System.Drawing.Point(121, 41);
+            this.txtProductId.Name = "txtProductId";
+            this.txtProductId.Size = new System.Drawing.Size(157, 22);
+            this.txtProductId.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Ürün Id:";
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(121, 261);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(73, 37);
+            this.btnList.TabIndex = 10;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // txtProductStock
+            // 
+            this.txtProductStock.Location = new System.Drawing.Point(121, 124);
+            this.txtProductStock.Name = "txtProductStock";
+            this.txtProductStock.Size = new System.Drawing.Size(157, 22);
+            this.txtProductStock.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 16);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Ürün Stok:";
+            // 
+            // txtProductPrice
+            // 
+            this.txtProductPrice.Location = new System.Drawing.Point(121, 163);
+            this.txtProductPrice.Name = "txtProductPrice";
+            this.txtProductPrice.Size = new System.Drawing.Size(157, 22);
+            this.txtProductPrice.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 16);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Ürün Fiyat:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 16);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Ürün Kategori:";
+            // 
+            // cmbProductCategory
+            // 
+            this.cmbProductCategory.FormattingEnabled = true;
+            this.cmbProductCategory.Location = new System.Drawing.Point(121, 200);
+            this.cmbProductCategory.Name = "cmbProductCategory";
+            this.cmbProductCategory.Size = new System.Drawing.Size(157, 24);
+            this.cmbProductCategory.TabIndex = 25;
+            // 
+            // btnListWithCategory
+            // 
+            this.btnListWithCategory.Location = new System.Drawing.Point(209, 261);
+            this.btnListWithCategory.Name = "btnListWithCategory";
+            this.btnListWithCategory.Size = new System.Drawing.Size(69, 37);
+            this.btnListWithCategory.TabIndex = 26;
+            this.btnListWithCategory.Text = "Listele";
+            this.btnListWithCategory.UseVisualStyleBackColor = true;
+            this.btnListWithCategory.Click += new System.EventHandler(this.btnListWithCategory_Click);
+            // 
+            // FrmProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1171, 570);
+            this.Controls.Add(this.btnListWithCategory);
+            this.Controls.Add(this.cmbProductCategory);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtProductPrice);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtProductStock);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtProductId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnList);
+            this.Name = "FrmProduct";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmProduct_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtProductId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.TextBox txtProductStock;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtProductPrice;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbProductCategory;
+        private System.Windows.Forms.Button btnListWithCategory;
+    }
+}
---
+++ FrmProduct.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkProject
+{
+    public partial class FrmProduct : Form
+    {
+        public FrmProduct()
+        {
+            InitializeComponent();
+        }
+        DbEntityProjectEntities db = new DbEntityProjectEntities();
+        void ProductList()
+        {
+            dataGridView1.DataSource = db.TblProduct.ToList();
+        }
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            ProductList();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            TblProduct tblProduct = new TblProduct();
+            tblProduct.ProductName = txtProductName.Text;
+            tblProduct.ProductStock = int.Parse(txtProductStock.Text);
+            tblProduct.ProductPrice = decimal.Parse(txtProductPrice.Text);
+            tblProduct.CategoryId = int.Parse(cmbProductCategory.SelectedValue.ToString());
+            db.TblProduct.Add(tblProduct);
+            db.SaveChanges();
+            ProductList();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var value = db.TblProduct.Find(int.Parse(txtProductId.Text));
+            db.TblProduct.Remove(value);
+            db.SaveChanges();
+            ProductList();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            var value = db.TblProduct.Find(int.Parse(txtProductId.Text));
+            value.ProductName = txtProductName.Text;
+            value.ProductStock = int.Parse(txtProductStock.Text);
+            value.ProductPrice = decimal.Parse(txtProductPrice.Text);
+            value.CategoryId = int.Parse(cmbProductCategory.SelectedValue.ToString());
+            db.SaveChanges();
+            ProductList();
+        }
+
+        private void FrmProduct_Load(object sender, EventArgs e)
+        {
+            var values = db.TblCategory.ToList();
+            cmbProductCategory.DisplayMember = "CategoryName";
+            cmbProductCategory.ValueMember = "CategoryId";
+            cmbProductCategory.DataSource = values;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var values = db.TblProduct.Where(x => x.ProductName == txtProductName.Text).ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnListWithCategory_Click(object sender, EventArgs e)
+        {
+            var values = db.TblProduct
+                .Join(db.TblCategory,
+                product => product.CategoryId,
+                category => category.CategoryId,
+                (product, category) => new
+                {
+                    ProductId = product.ProductId,
+                    ProductName = product.ProductName,
+                    ProductStock = product.ProductStock,
+                    ProductPrice = product.ProductPrice,
+                    CategoryId = category.CategoryId,
+                    CategoryName = category.CategoryName
+                })
+                .ToList();
+            dataGridView1.DataSource = values;
+
+        }
+    }
+}
